9157b00ec9349fa40991f2bd08e9b1fbbb520266
Layla Gammon <WS311472@weston.ac.uk>
2018-10-02 14:53:10 +0100

    Initial Notes on what to do :)

--- DoWhileLoopTsk3/DoWhileLoopTsk3.cpp
+++ DoWhileLoopTsk3/DoWhileLoopTsk3.cpp
@@ -1,7 +1,17 @@
-// DoWhileLoopTsk3.cpp : Defines the entry point for the console application.
-//
-
 #include "stdafx.h"
+#include "conio.h"
+#include <string>
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*Using other things such as CASE and IF
+Create a menu system that allows a user to register with the program or login
+They should not be given the option to login until they have registered with the system
+The same login rules apply after they have registered with the system
+When completed, a password recovery option should be added 
+This should have a secret question attached to it*/
 
 
 int main()
